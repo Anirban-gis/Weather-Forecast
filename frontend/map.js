a868bcd6ef518bf4e4586fb41ceb33a2f1a6b7b8
Anirban-gis <dasanirbangis1992@gmail.com>
2026-06-10 16:14:52 +0530

    Update map.js

--- frontend/map.js
+++ frontend/map.js
@@ -11,7 +11,7 @@ document.addEventListener("DOMContentLoaded", async function () {
     try {
 
         const response =
-            await fetch("https://weather-forecast-xqwe.onrender.com/api/latest");
+            await fetch("https://weather-forecast-xqwe.onrender.com/api/all-locations");
 
         const forecast =
             await response.json();
